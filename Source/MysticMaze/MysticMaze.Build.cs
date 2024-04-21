52d0caf4f619dde72e898dcfa3b66874ce387193
LeeTaes <apth1023@naver.com>
2024-04-21 12:50:26 +0900

    Init Project

---
+++ Source/MysticMaze/MysticMaze.Build.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class MysticMaze : ModuleRules
+{
+	public MysticMaze(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicIncludePaths.AddRange(new string[] { "MysticMaze" });
+
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
+	}
+}
---
+++ Source/MysticMaze/MysticMazeGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MysticMazeGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMysticMazeGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMysticMazeGameMode();
+};
+
+
+
