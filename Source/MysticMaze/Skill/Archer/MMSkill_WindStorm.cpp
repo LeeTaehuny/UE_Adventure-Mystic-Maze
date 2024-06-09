44d99ccafd637ea5522112df86662d9fafa00fae
LeeTaes <apth1023@naver.com>
2024-06-09 13:07:06 +0900

    궁수 윈드스톰 스킬 구현 (완)

---
+++ Source/MysticMaze/Skill/Archer/MMSkill_WindStorm.cpp
@@ -0,0 +1,103 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Skill/Archer/MMSkill_WindStorm.h"
+#include "Skill/MMSkillData.h"
+#include "Skill/MMStome.h"
+#include "Player/MMSkillComponent.h"
+#include "Interface/MMSkillInterface.h"
+#include "Collision/MMCollision.h"
+
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+UMMSkill_WindStorm::UMMSkill_WindStorm()
+{
+	static ConstructorHelpers::FObjectFinder<UAnimMontage> MontageRef(TEXT("/Script/Engine.AnimMontage'/Game/MysticMaze/Player/Animations/Skill/Archer/AM_WindStorm.AM_WindStorm'"));
+	if (MontageRef.Object)
+	{
+		SkillMontage = MontageRef.Object;
+	}
+}
+
+bool UMMSkill_WindStorm::UseSkill()
+{
+	bool Result = Super::UseSkill();
+
+	if (!Result) return false;
+
+	ACharacter* PlayerCharacter = Cast<ACharacter>(Owner);
+	if (PlayerCharacter && SkillMontage)
+	{
+		// 플레이어 이동 막기
+		PlayerCharacter->GetCharacterMovement()->SetMovementMode(EMovementMode::MOVE_None);
+
+		// 애님 몽타주 재생
+		PlayerCharacter->GetMesh()->GetAnimInstance()->Montage_Play(SkillMontage);
+
+		// 몽타주 재생 종료 바인딩
+		FOnMontageEnded EndDelegate;
+		EndDelegate.BindUObject(this, &UMMSkill_WindStorm::SkillEnd);
+		PlayerCharacter->GetMesh()->GetAnimInstance()->Montage_SetEndDelegate(EndDelegate, SkillMontage);
+	}
+
+	return Result;
+}
+
+void UMMSkill_WindStorm::SkillAttack()
+{
+	Super::SkillAttack();
+
+	// 토네이도 스폰 지점 구하기
+	FTransform Transform;
+	Transform.SetLocation(Owner->GetActorLocation() + (Owner->GetActorForwardVector() * 1000.0f) + (Owner->GetActorUpVector() * -45.0f));
+	Transform.SetRotation(Owner->GetActorRotation().Quaternion());
+	Transform.SetScale3D(FVector(3.0f));
+
+	// 토네이도 스폰
+	AMMStome* Storm = GetWorld()->SpawnActorDeferred<AMMStome>(AMMStome::StaticClass(), Transform);
+	// 시작 전 설정
+	Storm->SetOwner(Owner);
+	Storm->SetDamage(BaseDamage);
+	Storm->SetCriticalRate(CriticalRate);
+	// 스폰 완료
+	Storm->FinishSpawning(Transform);
+}
+
+void UMMSkill_WindStorm::Cancel()
+{
+	ACharacter* PlayerCharacter = Cast<ACharacter>(Owner);
+	if (PlayerCharacter && SkillMontage)
+	{
+		// 스킬 몽타주 재생 종료하기
+		PlayerCharacter->GetMesh()->GetAnimInstance()->Montage_Stop(0.1f, SkillMontage);
+
+		// 플레이어 이동 가능하게 설정
+		PlayerCharacter->GetCharacterMovement()->SetMovementMode(EMovementMode::MOVE_Walking);
+
+		// 스킬 종료 알림
+		IMMSkillInterface* SkillCharacter = Cast<IMMSkillInterface>(Owner);
+		if (Owner)
+		{
+			SkillCharacter->GetSkillComponent()->SetSkillEnd(SkillData->SkillName);
+		}
+	}
+}
+
+void UMMSkill_WindStorm::SkillEnd(UAnimMontage* Montage, bool IsEnded)
+{
+	ACharacter* PlayerCharacter = Cast<ACharacter>(Owner);
+	if (PlayerCharacter)
+	{
+		// 플레이어 이동 가능하게 설정
+		PlayerCharacter->GetCharacterMovement()->SetMovementMode(EMovementMode::MOVE_Walking);
+	}
+
+	// 스킬 종료 알림
+	IMMSkillInterface* SkillCharacter = Cast<IMMSkillInterface>(Owner);
+	if (Owner)
+	{
+		SkillCharacter->GetSkillComponent()->SetSkillEnd(SkillData->SkillName);
+	}
+}
---
+++ Source/MysticMaze/Skill/Archer/MMSkill_WindStorm.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Skill/MMSkillBase.h"
+#include "MMSkill_WindStorm.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYSTICMAZE_API UMMSkill_WindStorm : public UMMSkillBase
+{
+	GENERATED_BODY()
+	
+public:
+	UMMSkill_WindStorm();
+
+public:
+	virtual bool UseSkill() override;
+	virtual void SkillAttack() override;
+	virtual void Cancel() override;
+
+private:
+	void SkillEnd(class UAnimMontage* Montage, bool IsEnded);
+
+	UPROPERTY(VisibleAnywhere, Category = "Details")
+	TObjectPtr<class UAnimMontage> SkillMontage;
+};
